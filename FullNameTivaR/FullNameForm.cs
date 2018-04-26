87b2de1e8da38600c2096b1e5d96b5a608f1c994
TivaR8 <tiva.rait@ocsbstudent.ca>
2018-04-26 14:28:41 -0400

    Finished

--- FullNameTivaR/FullNameForm.cs
+++ FullNameTivaR/FullNameForm.cs
@@ -25,8 +25,53 @@ namespace FullNameTivaR
             InitializeComponent();
         }
 
+        // Procedure: PrintName
+        //Input: String firstName, String middleName, String lastName
+        // Output: void
+        // Description: This procedure displays a message box with the three parameters
+        public void PrintName(String firstName, String middleName, String lastName)
+        {
+            MessageBox.Show("Thank you, " + firstName + " " + middleName + " " + lastName);
+        }
+
+        //Procedure: PrintName
+        // Input: String firstName, String lastName
+        // Output: void
+        // Description: This procedure displays a message box with the two parameters
+        public void PrintName(String firstName, String lastName)
+        {
+            MessageBox.Show("Thank you, " + firstName + " " + lastName);
+        }
         private void btnEnter_Click(object sender, EventArgs e)
         {
+            // Declare Variables
+            String userFirstName, userMiddleName, userLastName;
+
+            // Get Names from text Boxes
+            userFirstName = this.txtFirstName.Text;
+            userMiddleName = this.txtMiddleName.Text;
+            userLastName = this.txtLastName.Text;
+
+            // Make the user enter a first name
+            if (userFirstName == "")
+            {
+                MessageBox.Show("Please enter your first name.", "Full Name Program");
+            }
+            // Make the user enter a last name
+            else if (userLastName == "")
+            {
+                MessageBox.Show("Please enter your last name.", "Full Name Program");
+            }
+            // If user enters no middle name call two parameter function
+            else if (userMiddleName == "")
+            {
+                PrintName(userFirstName, userLastName);
+            }
+            // If user enters a middle name call three parameter function
+            else
+            {
+                PrintName(userFirstName, userMiddleName, userLastName);
+            }
 
         }
     }
